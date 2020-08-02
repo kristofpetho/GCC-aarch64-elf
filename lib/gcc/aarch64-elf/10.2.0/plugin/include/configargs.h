2101c0e24c6426035c75b6553c38d51b034b9bc6
Kristof Petho <kristof.petho@gmail.com>
2020-08-02 15:02:39 +0200

    GCC 10.2.0 + Binutils 2.35

---
+++ lib/gcc/aarch64-elf/10.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/kristof/android/bldgcc/gcc/build/gcc-10.2.0/configure --target=aarch64-elf --enable-targets=all --prefix=/home/kristof/android/bldgcc/gcc/aarch64-elf-10.2.0 --enable-languages=c --without-headers --disable-bootstrap --disable-nls --disable-threads --disable-shared --disable-libmudflap --disable-libssp --disable-libgomp --disable-decimal-float --disable-libquadmath --disable-libatomic --disable-libcc1 --disable-libmpx --enable-checking=release --disable-shared --disable-nls --disable-threads --disable-tls --disable-bootstrap --disable-browser-plugin --disable-cloog-version-check --disable-isl-version-check --disable-libgomp --disable-libitm --disable-libmudflap --disable-libsanitizer --disable-libssp --disable-libstdc__-v3 --disable-multilib --disable-ppl-version-check --disable-sjlj-exceptions --disable-vtable-verify --disable-werror --enable-checking=release --enable-languages=c --enable-lto --enable-plugin --enable-gold --enable-graphite=yes --enable-initfini-array --enable-ld=default --with-target-system-zlib --with-newlib --with-gnu-as --with-gnu-ld --without-included-gettext --with-isl --with-gmp --with-mpfr --with-mpc";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
